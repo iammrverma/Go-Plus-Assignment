7faab1a97f85a708b8a3c620ba95f923768fabe8
RAJ <rajwebz2020@gmail.com>
2023-01-22 13:49:51 +0530

    saving like count to database model and adding some css to h3 tag

--- vote/urls.py
+++ vote/urls.py
@@ -7,5 +7,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('vote/like/', views.like, name='like'),
+    path('vote/dislike/', views.dislike, name='dislike'),
     path('', views.index, name='index'),
 ]
--- vote/views.py
+++ vote/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import VoteCount
+import json
 
-vote_count = VoteCount.objects.all()
+def like(request):
+    vote_count = VoteCount.objects.all()[0]
+    vote_count.likes += 1
+    vote_count.save()
+    data = {
+        'likes': vote_count.likes,
+        'dislikes': vote_count.dislikes
+    }
+    return HttpResponse(json.dumps(data))
+
+def dislike(request):
+    vote_count = VoteCount.objects.all()[0]
+    vote_count.dislikes += 1
+    vote_count.save()
+    data = {
+        'likes': vote_count.likes,
+        'dislikes': vote_count.dislikes
+    }
+    return HttpResponse(json.dumps(data))
 
 def index(request):
+    vote_count = VoteCount.objects.all()[0]
     data = {
-        'likes': vote_count[0].likes,
-        'dislikes': vote_count[0].dislikes
+        'likes': vote_count.likes,
+        'dislikes': vote_count.dislikes
     }
     return render(request, "vote/index.html", data)
